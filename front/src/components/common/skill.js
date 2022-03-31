a6b31f74c0c588bd82396372f6e65dfb77ca2396
Naayeon <naaay097@gmail.com>
2022-03-31 13:15:10 +0900

    중복되는 skill

--- front/src/components/common/skill.js
+++ front/src/components/common/skill.js
@@ -1,28 +1,9 @@
-import { Form } from 'react-bootstrap';
-
-function skillSelectForm () {
-  const skills =[{value: '', name:'선택안함'}, {value: 'Java'}, {value: 'Javasript'}, {value: 'jquery'},
+const skills =[{value: '', name:'선택안함'}, {value: 'Java'}, {value: 'Javasript'}, {value: 'jquery'},
   {value: 'Python'},{value: 'Html5'},{value: 'Css3'},{value: 'node.js'},
   {value: 'react'},{value: 'mongodb'},{value: 'mongoose'}, {value: 'django'},
   {value: 'mysql'}, {value: 'aws'}, {value: 'linux'}, {value: 'spring framework'}];
 
-   return (
-    <Form.Select
-    aria-label="Default select example" 
-    style={{
-      width: "200px",
-      marginBottom: "20px",
-      marginLeft: "20px"
-    }}
-    >
-      {skills.map((skill, index) => {
-        return (<option key={index} value={skill.value}>{skill.value || skill.name}</option>)
-       })
-        }
-    </Form.Select>
-  )
-}
+export default skills;
 
 
-export default skillSelectForm;
 
--- front/src/components/skill/CareerSkillAddEdit.js
+++ front/src/components/skill/CareerSkillAddEdit.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { Form, Button, Col, Row, Card } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Form, Button, Col, Row, Card} from 'react-bootstrap';
 import * as Api from "../../api";
 
-import SkillSelectForm from '../common/skill';
-import CareerSelectForm from '../common/career';
+import getSkill from "../common/Skill";
 
 const CareerSkillAddEdit = ({
   portfolioOwnerId,
   portfolioOwner,
-  isEditable,
+  isEditable, 
+  IsEditing, 
   skills,
   setSkill,
-  setOpen,
-  checkData }) => {
-
+  setOpen, 
+  checkData}) => {
+  
   const [career, setCareer] = useState("");
   const [language, setLanguage] = useState({
     language1: "",
-    language2: "",
+    language2:"",
     language3: ""
   })
 
@@ -25,7 +25,6 @@ const CareerSkillAddEdit = ({
 
   // language => post , languageList => get 
   const onChangeLanguage = (e) => {
-    console.log("ㅇㄹㅇ");
     setLanguage(cur => {
       return {
         ...cur,
@@ -38,6 +37,7 @@ const CareerSkillAddEdit = ({
   const onChangeCareer = (e) => {
     const value = e.target.value;
     setCareer(value);
+    console.log(career);
   }
 
   // 저장 (career, language data 없을때)
@@ -46,7 +46,7 @@ const CareerSkillAddEdit = ({
     const userId = portfolioOwnerId;
 
     console.log(languageList);
-    try {
+   try{
       await Api.post("skill/create", {
         portfolioOwner,
         userId,
@@ -57,21 +57,21 @@ const CareerSkillAddEdit = ({
       const res = await Api.get(`skilllist/${userId}`);
       setSkill(res.data);
     }
-    catch (error) {
+   catch(error){
       console.log(error);
       if (error.response) {
         const { data } = error.response;
         console.error("data : ", data);
+        }
       }
-    }
-    setOpen(false);
+    setOpen(false);  
   };
-
+  
   // 편집 (career, language data 가 존재하면 편집!)
   const handleEditSubmit = async (e) => {
     e.preventDefault();
     const userId = skills[0].userId;
-    try {
+   try{
       await Api.put(`skill/${skills[0].id}`, {
         userId,
         career,
@@ -83,95 +83,134 @@ const CareerSkillAddEdit = ({
       setSkill(res.data);
       console.log(skills);
     }
-    catch (error) {
+   catch(error){
       console.log(error);
       if (error.response) {
         const { data } = error.response;
         console.error("data : ", data);
+        }
       }
-    }
-    setOpen(false);
+    setOpen(false);  
   };
 
-  return (
-    <Card className="skillCard">
-      <Card.Body>
+
+   return (
+      <Card className="skillCard">
+        <Card.Body>
         <Form.Group>
-          <CareerSelectForm
-          key={career}
-          onChange={onChangeCareer} 
-          defaultValue={career}     
-           name="career"
-          ></CareerSelectForm>
-        </Form.Group>
-        <Card.Title className='text-start'>주요 기술</Card.Title>
-        <Form.Group>
-          <Row>
-            <SkillSelectForm
-            disabled={ isEditable === false ? true : false}
-            name="language1"
-            onChange={onChangeLanguage}
-            value={language.language1}       
-            ></SkillSelectForm>
-            <SkillSelectForm
-             disabled={ isEditable === false ? true : false}
-             name="language2"
-             onChange={onChangeLanguage}
-             value={language.language2}
-        ></SkillSelectForm>
-            <SkillSelectForm
-             disabled={ isEditable === false ? true : false}
-             name="language3"
-             onChange={onChangeLanguage}
-             value={language.language3}
-        ></SkillSelectForm>
-          </Row>
-        </Form.Group>
-        {checkData === true ?
-          (
-            <Col>
-              <Row className="mt-3 text-center text-info">
-                <Col sm={{ span: 20 }}>
-                  <Button
-                    mb="10"
-                    style={{
-                      border: "none",
-                      backgroundColor: "#339AF0",
-                      color: "#fffff"
-                    }}
-                    size="sm"
-                    onClick={handleEditSubmit}
-                  >
-                    편집
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          ) :
-          (
-            <Col>
-              <Row className="mt-3 text-center text-info">
-                <Col sm={{ span: 20 }}>
-                  <Button
-                    mb="10"
-                    style={{
-                      border: "none",
-                      backgroundColor: "#339AF0",
-                      color: "#fffff"
-                    }}
-                    size="sm"
-                    onClick={handleSubmit}
-                  >
-                    추가
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          )
+     <Card.Title className='text-start'>경력</Card.Title>
+     <Form.Select aria-label="Default select example" 
+     disabled={ isEditable === false ? true : false}
+     key={career}
+     onChange={onChangeCareer} 
+     defaultValue={career}
+    style={{
+      width: "200px",
+      marginBottom: "20px"
+    }}>
+      <option value=''>선택안함</option>
+      <option value="1~2">1~2년</option>
+      <option value="3~4">3~4년</option>
+      <option value="5~6">5~6년</option>
+      <option value="7~8">7~8년</option>
+     </Form.Select>
+     </Form.Group>
+     <Card.Title className='text-start'>주요 기술</Card.Title>
+     <Form.Group>
+     <Row>
+     <Form.Select 
+     aria-label="Default select example" 
+     disabled={ isEditable === false ? true : false}
+     name="language1"
+     onChange={onChangeLanguage}
+     value={language.language1}
+    style={{
+      width: "200px",
+      marginBottom: "20px",
+      marginLeft: "13px"
+    }}>
+    {  getSkill.map((skill, index) => {
+      return (<option key={index} value={skill.value}>{skill.value || skill.name}</option>)
+       })
         }
-      </Card.Body>
-    </Card>
-  )
+      </Form.Select>
+      <Form.Select aria-label="Default select example" 
+      disabled={ isEditable === false ? true : false}
+      name="language2"
+      value={language.language2}
+      onChange={onChangeLanguage}
+      style={{
+      width: "200px",
+      marginBottom: "20px",
+      marginLeft: "20px"
+      }}>
+    {  getSkill.map((skill, index) => {
+         return (<option key={index} value={skill.value}>{skill.value || skill.name}</option>)
+       })
+        }
+      </Form.Select>
+      <Form.Select aria-label="Default select example" 
+     disabled={ isEditable === false ? true : false}
+     name="language3"
+     value={language.language3}
+      onChange={onChangeLanguage}
+    style={{
+      width: "200px",
+      marginBottom: "20px",
+      marginLeft: "20px"
+    }}>
+       {getSkill.map((skill, index) => {
+        return (<option key={index} value={skill.value}>{skill.value || skill.name}</option>)
+       })
+        }
+      </Form.Select>
+     </Row>
+     </Form.Group>
+     {checkData === true ?
+     (
+          <Col>
+            <Row className="mt-3 text-center text-info">
+              <Col sm={{ span: 20 }}>
+              <Button
+               mb="10"
+               style={{
+                border:"none",
+                backgroundColor:"#339AF0",
+                color: "#fffff"
+                }} 
+                size="sm"
+                onClick={handleEditSubmit}
+                >
+                  편집
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        ) : 
+        (
+          <Col>
+          <Row className="mt-3 text-center text-info">
+            <Col sm={{ span: 20 }}>
+            <Button
+             mb="10"
+             style={{
+              border:"none",
+              backgroundColor:"#339AF0",
+              color: "#fffff"
+              }} 
+              size="sm"
+              onClick={handleSubmit}
+              >
+                추가
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+        )
+        }
+        </Card.Body>
+     </Card>
+   )
 }
 
 export default CareerSkillAddEdit;
